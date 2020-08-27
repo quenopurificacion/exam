eb24ca0b614ca5db1428fc2a5f692125c3a6e663
Queno Purificacion <quian0108@gmail.com>
2020-08-28 10:09:58 +1200

    Update app.component.ts
    Updating codes with comments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,29 +13,35 @@ export class AppComponent implements OnInit {
     constructor(
         private service : ExamService
     ) {}
-
+    
+    //Initialize data
     ngOnInit(): void {
         this.getPostFromPostAPI()
     }
 
+    //Setup parameter for "Posts" to get it's data
     getPostFromPostAPI() {
         this.getAPIResponse('posts', 'posts', '')
     }
 
+    //Get comments one by one using the 'posts' data
     getCommentForEachPost(posts) {
         this.loop(posts)
     }
 
+    //Loop posts to get it's comment
     loop(posts) {
         posts.forEach(post => {
             this.getComment(post)
         })
     }
 
+    //Get comment by using 'post.id' from the API, we are using one function to get the data by just chaning the parameter
     getComment(post) {
         this.getAPIResponse('comments?postId='+post.id, 'comments', post)
     }
 
+    //This is the function that handles the API, we can use it in different API by just changing parameter and adding some condition
     getAPIResponse(param, from, post) {
 
         this.service
@@ -44,11 +50,13 @@ export class AppComponent implements OnInit {
         .subscribe(response => {
 
             if(from === 'comments') {
+                //This is the initialized data and once page load, it will display this object data
                 this.data.push({
                     post: post,
                     comment: response
                 })
             } else {
+                //This will get the comment one by one and assign it everytime it has a new response, and by doing this, we will get the exact ID that we need for the comment.
                 this.getCommentForEachPost(response)
             }
             
